dc6d3e1eebc29f601bb85858d5daa77a11119abf
Bruna <brunasilv126@gmail.com>
2023-05-10 15:03:59 -0300

    get game by id and exceptions

--- src/main/java/com/bruna/dslist/controllers/GamesController.java
+++ src/main/java/com/bruna/dslist/controllers/GamesController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bruna.dslist.dto.GamesDTO;
 import com.bruna.dslist.dto.GamesMinDTO;
 import com.bruna.dslist.services.GamesService;
 
@@ -17,11 +20,16 @@ public class GamesController {
 	@Autowired
 	private GamesService gamesService;
 	
+	@GetMapping(value = "/{id}")
+	public GamesDTO findById(@PathVariable Long id) {
+		GamesDTO result = gamesService.findById(id);
+		return result;
+	}
+	
 	@GetMapping
 	public List<GamesMinDTO> findAll() {
 		List<GamesMinDTO> result = gamesService.findAll();
 		return result;
-		
 	}
 
 }
--- src/main/java/com/bruna/dslist/services/GamesService.java
+++ src/main/java/com/bruna/dslist/services/GamesService.java
@@ -3,8 +3,11 @@ package com.bruna.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.bruna.dslist.dto.GamesDTO;
 import com.bruna.dslist.dto.GamesMinDTO;
 import com.bruna.dslist.entities.Games;
 import com.bruna.dslist.repositories.GamesRepository;
@@ -15,9 +18,28 @@ public class GamesService {
 	@Autowired
 	private GamesRepository gameRepository;
 	
+	public class NotFoundException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public NotFoundException(String message) {
+	        super(message);
+	    }
+	}
+
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public GamesDTO findById(Long id) {
+		try {
+			Games result = gameRepository.findById(id).get();
+			return new GamesDTO(result);
+		} catch (EmptyResultDataAccessException ex) {
+	        throw new NotFoundException ("Game not found with ID: " + id);
+		}
+		
+	}
+
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GamesMinDTO> findAll() {
 		List<Games> result = gameRepository.findAll();
 		return result.stream().map(x -> new GamesMinDTO(x)).toList();
-
 	}
 }
